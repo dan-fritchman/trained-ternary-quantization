70a93c50c5f0c3c4526c5ec8be6eb67d1da84057
Brian Liao <phyred23@gmail.com>
2020-03-01 21:02:47 -0800

    quick git commit

--- ttq_microbotnet/get_microbotnet.py
+++ ttq_microbotnet/get_microbotnet.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 
 import sys
+sys.path.append('../vanilla_microbotnet/')
 from fd_mobilenet_v3 import FdMobileNetV3Imp2
 
 
@@ -62,6 +63,60 @@ def get_model(learning_rate=1e-3, width_multiplier=0.32):
         {'params': bn_weights},
         {'params': bn_biases}
     ]
+    #params: parameter group to train seperately
+    optimizer = optim.Adam(params, lr=learning_rate)
+
+    loss = nn.CrossEntropyLoss().cuda()
+    model = model.cuda()  # move the model to gpu
+    return model, loss, optimizer
+
+def get_model2(learning_rate=1e-3, width_multiplier=0.32):
+
+    model = FdMobileNetV3Imp2(classes_num=10, input_size=32, width_multiplier=width_multiplier, mode='small')
+
+    # first layer is not trainable
+    first_layer = model.features[0][0]
+    first_layer.weight.requires_grad = False
+
+    # last fc layer is not trainable. (NOTE: there are multiple Linear units
+    # in the bottleneck layers. What should we do with them?)
+    last_linear = model.classifier[1]
+    weights = [last_linear.weight]
+    biases = [last_linear.bias]
+
+
+    # [p for n, p in model.named_parameters() if 'fc' in n]
+    # what about bias
+
+
+    # Only bottlenecks [1:11] are quantized
+    '''
+    weights_to_be_quantized = [
+        p for n, p in model.features[1:11].named_parameters()
+        if 'conv' in n and 'lastBN' not in n and 'fc' not in n
+    ]
+    '''
+    weights_to_be_quantized = []
+    # parameters of batch_norm layers
+    bn_weights = [model.features[0][1].weight, model.features[11][1].weight] + \
+        [
+            p for n, p in model.features[1:11].named_parameters()
+            if 'lastBN' in n and 'weight' in n
+        ]
+    bn_biases = [model.features[0][1].bias, model.features[11][1].bias] + \
+        [
+            p for n, p in model.features[1:11].named_parameters()
+            if 'lastBN' in n and 'bias' in n
+        ]
+
+    params = [
+        {'params': weights, 'weight_decay': 1e-4},
+        {'params': weights_to_be_quantized},
+        {'params': biases},
+        {'params': bn_weights},
+        {'params': bn_biases}
+    ]
+    #params: parameter group to train seperately
     optimizer = optim.Adam(params, lr=learning_rate)
 
     loss = nn.CrossEntropyLoss().cuda()
